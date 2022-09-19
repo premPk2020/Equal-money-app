979ff2f71cb285d17ccec740dcf6da603d8ae38e
Prem <premkumarcse2020@gmail.com>
2022-09-19 11:52:45 +0530

    Amount Input Field Finished

--- app/src/main/java/com/orange/equal/MainActivity.kt
+++ app/src/main/java/com/orange/equal/MainActivity.kt
@@ -3,12 +3,13 @@ package com.orange.equal
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.orange.equal.ui.theme.EqualTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    EqualApp()
                 }
             }
         }
@@ -30,6 +31,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun EqualApp() {
+    Column(Modifier.padding(20.dp)) {
+        AmountDisplay()
+        Spacer(modifier = Modifier.height(20.dp))
+        InputAmount()
+    }
 
 }
 
